5f39dc5703fbf18c94c01e2ffda55b1cf95974ea
Steven Roper <sroper@masteryconnect.com>
2016-06-07 08:39:20 -0600

    Create tests to understand immutable data.

---
+++ test/immutable_spec.js
@@ -0,0 +1,44 @@
+import {expect} from 'chai';
+import {List} from 'immutable';
+
+describe('immutability', () => {
+  
+  describe('a number', () => {
+    
+    function increment(num) {
+      return num + 1;
+    }
+
+    it('is immutable', () => {
+      let state = 42;
+      let nextState = increment(state);
+
+      expect(nextState).to.equal(43);
+      expect(state).to.equal(42);
+    });
+ 
+  });
+
+  describe('a List', () => {
+    
+    function addMovie(currentState, movie) {
+      return currentState.push(movie);
+    }
+
+    it('is immutable', () => {
+      let state = List.of('Trainspotting', '28 Days Later');
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+      expect(state).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later'
+      ));
+    });
+  });
+
+});
